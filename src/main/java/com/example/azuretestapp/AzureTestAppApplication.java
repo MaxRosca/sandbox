0f6f32336d4a8312d6b0ec158ffcd3517004246d
roscamax <roscamaxim2002@gmail.com>
2023-08-31 16:38:40 +0200

    initial commit

---
+++ src/main/java/com/example/azuretestapp/AzureTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.azuretestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/azuretestapp/AzureTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azuretestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
